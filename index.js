2589bd0b1e222ad3ced0b06ccf144a22a52573a7
calvin <calvin.francis96@outlook.com>
2021-03-19 09:42:04 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messagesArray = []
+    for (let i = 0; i < names.length; i++) {
+        let greeting = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messagesArray[i] = greeting;
+    }
+    return messagesArray;
+}
+const names = ["Ada", "Brendan", "Ali"];
+const avent = "birthday";
+
+function countDown(integer) {
+    let counter = integer
+    while (counter >= 0) {
+        console.log(counter);
+        counter--
+    }
+}
